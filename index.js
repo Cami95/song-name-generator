2dd55f473c8d96ac5a73777ecda569d1473f7702
Camille (Cami) Johnson <74115479+Cami95@users.noreply.github.com>
2025-08-12 09:49:00 -0400

    added axios to the head

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-function getSong(event) {
-  event.preventDefault()
 
-
+function gettingSong (response) {
 
   new Typewriter("#song", {
-    strings: "Song Lyrics",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: ""
     
   });
-  
 
-  
+}
+
+
+
+function getSong(event) {
+  event.preventDefault()
+
+
+  let apiKey = "d0abb40f949d3e5e54983cob5a3ct305"
+  let prompt = "Please give me the tile of a song, who wrote it,  and the song lyrics. Have the song title on top, below that have the name of the song writter, and below that will be the lyrics"
+  let context = "You are a world famous DJ who can easily find a song the relates to any emotion"
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+
+
+  axios.get(apiURL).then(gettingSong)
+
+
 }
 
 
